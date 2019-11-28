30a56bab68017cb4848aec9321ec04f98d1f7a47
yguivarc <>
2019-11-28 12:42:16 +0100

    suppr import useless

--- hidoop/src/config/Project.java
+++ hidoop/src/config/Project.java
@@ -2,5 +2,6 @@ package config;
 
 public class Project {
 
-    public static String PATH = "";
+    public final static String PATH = "";
+    public final static String[] HOSTS = {"bore", "azote", "antimoine"};
 }
