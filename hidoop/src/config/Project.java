51e27f1465980b1d0eb45a2ff3f6e7dd714834cd
Vernizeau Thomas <tvernize@pixie.enseeiht.fr>
2020-01-10 16:38:49 +0100

    modification de l'host pour le namingNode

--- hidoop/src/config/Project.java
+++ hidoop/src/config/Project.java
@@ -3,7 +3,7 @@ package config;
 public class Project {
 
     public final static String PATH = "/tmp";
-    public final static String[] HOSTS =  {"localhost","localhost", "localhost", "localhost"};//{"localhost","beatles", "bouba", "albator"};
+    public final static String[] HOSTS =  {"localhost","localhost", "localhost", "localhost"};//{"phenix","beatles", "bouba", "albator"};
     public final static Integer[] HOSTSPORT = {4000,4001,4002,4003};
     public final static String NAMINGNODE = "localhost"; //"fluor";
     public final static Integer REGISTRYPORT = 12345;
