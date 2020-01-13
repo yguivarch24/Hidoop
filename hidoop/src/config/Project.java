d3528ee921eebefd303e45ef0a3ff78c5fa1bb4d
yguivarc <>
2020-01-13 16:01:54 +0100

    Test différent pc hdfs modif Projet map pas encore testé mauvait format

--- hidoop/src/config/Project.java
+++ hidoop/src/config/Project.java
@@ -13,14 +13,12 @@ import java.rmi.registry.LocateRegistry;
 public class Project {
 
     public final static String PATH = "/tmp/";
-    public final static String[] HOSTS =  {"localhost","localhost"/*, "localhost", "localhost"*/};
-    //public final static String[] HOSTS =  {"localhost","localhost"};//{"phenix","beatles", "bouba", "albator"};
-    public final static Integer[] HOSTSPORT = {4000,4001,/*4002,4003*/};
-    public final static Integer[] DAEMONPORTS = {4004,4005,4006,4007};
+    public final static String[] HOSTS =  {"eowyn","arwen"}; //{"localhost", "localhost", "localhost", "localhost"};
+    public final static Integer[] HOSTSPORT = {4001, 4001};//{4000,4001,4002,4003};
 
     public final static int TAILLEPART = 5*1024;
 
-    public final static String NAMINGNODE = "localhost"; //"fluor";
+    public final static String NAMINGNODE = "eowyn"; //"localhost"; //"fluor";
     public final static Integer REGISTRYPORT = 12345;
 
 
