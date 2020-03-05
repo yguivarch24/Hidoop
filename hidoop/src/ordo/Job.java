caacce9fe3aee53058ec7c83b410b4948877beaa
tvernize <thomas.vernizeau@etu.enseeiht.fr>
2020-03-05 17:38:07 +0100

    Le nom du fichier de sortie ainsi que le type du fichier d'entrée sont de nouveau optionnels (pour de vrai)

--- hidoop/src/ordo/Job.java
+++ hidoop/src/ordo/Job.java
@@ -6,6 +6,7 @@ import hdfs.*;
 
 import java.io.File;
 import java.net.MalformedURLException;
+import java.nio.file.Paths;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
@@ -22,6 +23,8 @@ public class Job implements JobInterfaceX {
     private Format.Type outFormat;
     private String inFName;
     private String outFName;
+    private String inFPath;
+    private String outFPath;
     private int nbReduces;
     private int nbMaps;
     private SortComparator sortComp;
@@ -29,21 +32,25 @@ public class Job implements JobInterfaceX {
     public Job() {
         this.inFName = "";
         this.outFName = "";
+        this.inFPath = "";
+        this.outFPath = "";
         this.inFormat = Format.Type.LINE;
         this.outFormat = Format.Type.KV;
     }
 
     Job(String infname, String outfname, Format.Type inForm) {
-        this.inFName = infname;
-        this.outFName = outfname;
+        this.inFName = Paths.get(infname).getFileName().toString();
+        this.outFName = Paths.get(outfname).getFileName().toString();
+        this.inFPath = infname;
+        this.outFPath = outfname;
         this.inFormat = inForm;
         this.outFormat = Format.Type.KV;
     }
 
     public void startJob(MapReduce mr) throws RemoteException, NotBoundException, MalformedURLException, InterruptedException {
 
-        if (this.outFName.equals("")) { // si aucun nom pour le fichier de sortie n'à été donné, on met un nom par défaut
-            this.outFName = this.inFName + "-KVres";
+        if (this.outFPath.equals("")) { // si aucun nom pour le fichier de sortie n'à été donné, on met un nom par défaut
+            this.outFPath = this.inFPath + "-KVres";
         }
 
         CallBackImpl[] cb = new CallBackImpl[Project.HOSTS.length]; // 1 CallBack par hosts
@@ -196,20 +203,21 @@ public class Job implements JobInterfaceX {
         HdfsClient.HdfsRead(this.inFName, Project.PATH + this.inFName + "-res");
 
         Thread thread = HdfsClient.HdfsDelete(this.inFName); // suppréssion des fragments désormais inutiles sur les serveurs
+
         Format readerReduce;
         Format writerReduce;
         switch (this.outFormat) { // initialisation du reader pour le fichier résultant des traitement et du writer pour le fichier de sortie de Hidoop
             case LINE :
                 readerReduce = new LineFormat(Project.PATH + this.inFName + "-res");
-                writerReduce = new LineFormat(this.outFName);
+                writerReduce = new LineFormat(this.outFPath);
                 break;
             case KV :
                 readerReduce = new KVFormat(Project.PATH + this.inFName + "-res");
-                writerReduce = new KVFormat(this.outFName);
+                writerReduce = new KVFormat(this.outFPath);
                 break;
             default :
                 readerReduce = new KVFormat(Project.PATH + this.inFName + "-res");
-                writerReduce = new KVFormat(this.outFName);
+                writerReduce = new KVFormat(this.outFPath);
                 break;
         }
 
