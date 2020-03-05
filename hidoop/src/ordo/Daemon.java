7b558bebbadcc8541fc9961fc0c3eba2f97d27a5
aaminebha <ahmedamine.belhajali@gmail.com>
2020-03-05 17:00:13 +0100

    modification d'appel du runMap : Les daemons reçoivent désormais une liste des reader/writers dont ils auront besoin pour traiter leurs fragments.

--- hidoop/src/ordo/Daemon.java
+++ hidoop/src/ordo/Daemon.java
@@ -2,10 +2,11 @@ package ordo;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.util.List;
 
 import map.Mapper;
 import formats.Format;
 
 public interface Daemon extends Remote {
-	public void runMap (Mapper m, Format reader, Format writer, CallBack cb) throws RemoteException;
+	public void runMap (Mapper m, List<Format> reader, List<Format> writer, CallBack cb) throws RemoteException;
 }
--- hidoop/src/ordo/Job.java
+++ hidoop/src/ordo/Job.java
@@ -6,13 +6,13 @@ import hdfs.*;
 
 import java.io.File;
 import java.net.MalformedURLException;
-import java.nio.file.Paths;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.concurrent.Semaphore;
 import map.*;
 
@@ -22,8 +22,6 @@ public class Job implements JobInterfaceX {
     private Format.Type outFormat;
     private String inFName;
     private String outFName;
-    private String inFPath;
-    private String outFPath;
     private int nbReduces;
     private int nbMaps;
     private SortComparator sortComp;
@@ -31,17 +29,13 @@ public class Job implements JobInterfaceX {
     public Job() {
         this.inFName = "";
         this.outFName = "";
-        this.inFPath = "";
-        this.outFPath = "";
         this.inFormat = Format.Type.LINE;
         this.outFormat = Format.Type.KV;
     }
 
     Job(String infname, String outfname, Format.Type inForm) {
-        this.inFName = Paths.get(infname).getFileName().toString();
-        this.outFName = Paths.get(outfname).getFileName().toString();
-        this.inFPath = infname;
-        this.outFPath = outfname;
+        this.inFName = infname;
+        this.outFName = outfname;
         this.inFormat = inForm;
         this.outFormat = Format.Type.KV;
     }
@@ -50,9 +44,6 @@ public class Job implements JobInterfaceX {
 
         if (this.outFName.equals("")) { // si aucun nom pour le fichier de sortie n'à été donné, on met un nom par défaut
             this.outFName = this.inFName + "-KVres";
-        }
-        if (this.outFPath.equals("")) { // si aucun chemin pour le fichier de sortie n'a été donné, on met un chemin par défaut
-            this.outFPath = this.inFPath + "-KVres";
         }
 
         CallBackImpl[] cb = new CallBackImpl[Project.HOSTS.length]; // 1 CallBack par hosts
@@ -66,15 +57,15 @@ public class Job implements JobInterfaceX {
             }
         }
 
-        Format reader; // pour lire les fragments
-        Format writer; // pour écrire le résultat du traitement des fragments
+
 
         HashMap<String, ArrayList<String>> maps; // pour stocker la liste des fragments sur chaque host
         maps = ((FragmentListInter) Naming.lookup("//" + Project.NAMINGNODE + ":" + Project.REGISTRYPORT + "/list")).getFragments();
             // on récupère les noms des fragments pour chaque hosts sur le registry du NamingNode
-
+        List<Format> reader =new ArrayList<>();
+        List<Format> writer =new ArrayList<>();
         long startTime = System.currentTimeMillis();
-        for (int j = 0; j < maxLength(maps); j++) { // une boucle par fragment (1 fragment traité sur chaque host)
+        /*for (int j = 0; j < maxLength(maps); j++) { // une boucle par fragment (1 fragment traité sur chaque host)
             for (int i = 0; i < Project.HOSTS.length; i++) { // une boucle par host
 
                 String keyHost = Project.HOSTS[i] + ":" + Project.HOSTSPORT[i];
@@ -91,7 +82,7 @@ public class Job implements JobInterfaceX {
                             default :
                                 reader = new LineFormat(maps.get(keyHost).get(j));
                         }
-                        
+
                         switch (this.outFormat) { // initialisation du writer à partir du nom récupérer depuis NamingNode suivi de -res pour le différencier d'un fragment non traité
                             case LINE :
                                 writer = new LineFormat(maps.get(keyHost).get(j) + "-res");
@@ -125,18 +116,78 @@ public class Job implements JobInterfaceX {
                     }
                 }
             }
-            
-            for (int i = 0; i < Project.HOSTS.length; i++) { // on attend le CallBack de chaque host ayant sur lesquels un runMap à été lancé
-                if (wait[i]) { // si le CallBack de cet host est attendu
-                    try {
-                        cb[i].mapFini.acquire(); // si le CallBack a déja été reçu, on passe, sinon on l'attend
-                        wait[i] = false; // aucuns runMap ne tourne désormais sur cet host, on ne cherchera pas à l'attendre tant qu'aucun autre runMap n'aura été lancé dessus
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
+
+
+        }*/
+
+        for (int i = 0; i < Project.HOSTS.length; i++) { // une boucle par host
+            String keyHost = Project.HOSTS[i] + ":" + Project.HOSTSPORT[i];
+            reader =new ArrayList<>();
+            writer =new ArrayList<>();
+            List<String> frags=maps.get(keyHost);
+            String host=Project.HOSTS[i];
+            for(int j=0;j<frags.size();j++){
+                try {
+                    switch (this.inFormat) { // initialisation du reader à partir du nom récupérer depuis NamingNode
+                        case LINE :
+                            reader.add(new LineFormat(frags.get(j)));
+                           // System.out.println("dans le switch"+frags.get(j));
+                            break;
+                        case KV :
+                            reader.add(new KVFormat(frags.get(j)));
+                            break;
+                        default :
+                            reader.add(new LineFormat(frags.get(j)));
                     }
+                    //System.out.println("les frags est "+maps.get(keyHost).get(j));
+                    switch (this.outFormat) { // initialisation du writer à partir du nom récupérer depuis NamingNode suivi de -res pour le différencier d'un fragment non traité
+                        case LINE :
+                            writer.add(new LineFormat(frags.get(j) + "-res"));
+                            break;
+                        case KV :
+                            writer.add(new KVFormat(frags.get(j) + "-res"));
+                            break;
+                        default :
+                            writer.add(new KVFormat(frags.get(j) + "-res"));
+                    }
+
+                    // création de copies constantes des variables pour le lancement du runMap dans un thread (pour éviter blocage)
+
+                    //System.out.println(reader.get(j).getFname());
+
+                    //new Thread(() -> {
+                    //        try {
+                    //((Daemon) Naming.lookup("//" + Project.HOSTS[num] + ":" + Project.REGISTRYPORT + "/Daemon")).runMap(mapRed, read, write, caba[num]); // on récupère le ième Daemon et on lance le map
+                    //        } catch (NotBoundException | MalformedURLException | RemoteException e) {
+                    //            throw new RuntimeException(e.getMessage());
+                    //        }
+                    //    }).start(); // lancement du thread
+
+                    wait[i] = true; // un runMap à été lancé, il faudra attendre son CallBack
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            final int num = i;
+            final String Host=host;
+            final MapReduce mapRed = mr;
+            final List<Format> read = reader;
+            final List<Format> write = writer;
+            final CallBackImpl[] caba = cb;
+            ((Daemon) Naming.lookup("//" + Host + ":" + Project.REGISTRYPORT + "/Daemon")).runMap(mapRed, read, write, caba[num]); // on récupère le ième Daemon et on lance le map
+        }
+
+        for (int i = 0; i < Project.HOSTS.length; i++) { // on attend le CallBack de chaque host ayant sur lesquels un runMap à été lancé
+            if (wait[i]) { // si le CallBack de cet host est attendu
+                try {
+                    cb[i].mapFini.acquire(); // si le CallBack a déja été reçu, on passe, sinon on l'attend
+                    wait[i] = false; // aucuns runMap ne tourne désormais sur cet host, on ne cherchera pas à l'attendre tant qu'aucun autre runMap n'aura été lancé dessus
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
             }
         }
+
         long endTime   = System.currentTimeMillis();
         long totalTime = endTime - startTime;
         System.out.println("Temps de Mapping : " + totalTime);
@@ -145,24 +196,25 @@ public class Job implements JobInterfaceX {
         HdfsClient.HdfsRead(this.inFName, Project.PATH + this.inFName + "-res");
 
         Thread thread = HdfsClient.HdfsDelete(this.inFName); // suppréssion des fragments désormais inutiles sur les serveurs
-
+        Format readerReduce;
+        Format writerReduce;
         switch (this.outFormat) { // initialisation du reader pour le fichier résultant des traitement et du writer pour le fichier de sortie de Hidoop
             case LINE :
-                reader = new LineFormat(Project.PATH + this.inFName + "-res");
-                writer = new LineFormat(this.outFPath);
+                readerReduce = new LineFormat(Project.PATH + this.inFName + "-res");
+                writerReduce = new LineFormat(this.outFName);
                 break;
             case KV :
-                reader = new KVFormat(Project.PATH + this.inFName + "-res");
-                writer = new KVFormat(this.outFPath);
+                readerReduce = new KVFormat(Project.PATH + this.inFName + "-res");
+                writerReduce = new KVFormat(this.outFName);
                 break;
             default :
-                reader = new KVFormat(Project.PATH + this.inFName + "-res");
-                writer = new KVFormat(this.outFPath);
+                readerReduce = new KVFormat(Project.PATH + this.inFName + "-res");
+                writerReduce = new KVFormat(this.outFName);
                 break;
         }
 
         startTime = System.currentTimeMillis();
-        mr.reduce(reader, writer); // Traitement du fichier résultant des traitements des fragments
+        mr.reduce(readerReduce, writerReduce); // Traitement du fichier résultant des traitements des fragments
         endTime   = System.currentTimeMillis();
         totalTime = endTime - startTime;
         System.out.println("Temps du Reduce : " + totalTime);
