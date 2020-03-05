f78febaeae89c2570c53f8195d25dac2a7e32540
yguivarc <>
2020-03-05 16:48:42 +0100

    Modification script clean

--- hidoop/config/clean_all.sh
+++ hidoop/config/clean_all.sh
@@ -9,11 +9,12 @@ if [ $# = 1 ]; then
     namingNode=$(cat "$fileNamingNode")
     IFS=':' read -ra host <<< "$namingNode"
     ssh "$1"@"${host[0]}" "kill -9 \`ps -C java -o pid=\`"
-
+    ssh "$1"@"${host[0]}" "rm -r /tmp/${host[1]}"
     while IFS= read -r line
     do
       IFS=':' read -ra host <<< "$line"
       ssh "$1"@"${host[0]}" "kill -9 \`ps -C java -o pid=\`" < /dev/null
+      ssh "$1"@"${host[0]}" "rm -r /tmp/${host[1]}" < /dev/null
     done <"$fileServeur"
 
 else
