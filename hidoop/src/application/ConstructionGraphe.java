39306a59e9155280079473f9122a91ead932e788
vdurand <victor.durand.mlt@gmail.com>
2020-04-09 14:39:52 +0200

    Continuation ConstructionGraphe

--- hidoop/src/application/ConstructionGraphe.java
+++ hidoop/src/application/ConstructionGraphe.java
@@ -1,4 +1,25 @@
 package application;
 
-public class ConstructionGraphe {
+import formats.FormatReader;
+import formats.FormatWriter;
+import map.MapReduce;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ConstructionGraphe implements MapReduce {
+
+
+    /**
+     * void map: pour chaque page considérée (lien présent sur la page d'origine), extrait les liens figurants dans la page considérée
+     * @param reader lecteur qui va récupérer toutes les pages à considérer
+     * @param writer écrit l'ensemble des clés valeurs déstinées au traitement de PageRank, de la forme < URL, (PR,liste_liens) >
+     */
+    public void map(FormatReader reader, FormatWriter writer) {
+        Map<String,Integer> hm = new HashMap<>();
+    }
+
+    public void reduce(FormatReader reader, FormatWriter writer) {
+
+    }
 }
